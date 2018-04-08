a397bdb7b5340d973e88602e5487d99dbe32f6e1
Vittorio Lupinetti <lupinettivittorio@gmail.com>
2018-04-08 22:13:52 +0200

    bugfix wsgi

--- aruta/wsgi.py
+++ aruta/wsgi.py
@@ -7,7 +7,10 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 """
 
-import os
+import os,sys
+
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)),'..'))
 
 from django.core.wsgi import get_wsgi_application
 
